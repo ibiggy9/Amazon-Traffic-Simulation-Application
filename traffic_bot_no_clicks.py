81daa47c4378192195a5c88d61050f9e78dc1b71
Main <main@Mains-MBP.hitronhub.home>
2021-05-10 09:09:33 -0400

    Debugging Chromedriver crash issue

--- traffic_bot_no_clicks.py
+++ traffic_bot_no_clicks.py
@@ -17,45 +17,56 @@ from http_request_randomizer.requests.proxy.requestProxy import RequestProxy
 
 class Scrape:
     def __init__(self, url):
-        '''
-        self.req_proxy = RequestProxy()
-        self.proxies = self.req_proxy.get_proxy_list()
-        self.proxy_list = []
-        for prox in self.proxies:
-            if prox.country == 'Canada':
-                self.proxy_list.append(prox)
-        self.proxy = self.proxy_list[0].get_address()
+        # Proxy Server
+        try:
+            self.req_proxy = RequestProxy()
+            self.proxies = self.req_proxy.get_proxy_list()
+            self.proxy_list = []
+            for prox in self.proxies:
+                if prox.country == 'Canada':
+                    self.proxy_list.append(prox)
+            self.proxy = self.proxy_list[0].get_address()
+            
+            self.proxy_run = webdriver.DesiredCapabilities.CHROME['proxy']={
+                'httpProxy': self.proxy,
+                'ftpProxy': self.proxy,
+                'sslProxy':self.proxy,
+
+                "proxyType":"MANUAL",
+
+
+            }
+        except: 
+            pass
         
-        self.proxy_run = webdriver.DesiredCapabilities.CHROME['proxy']={
-            'httpProxy': self.proxy,
-            'ftpProxy': self.proxy,
-            'sslProxy':self.proxy,
-
-            "proxyType":"MANUAL",
-
-
-        }
-        '''
-        self.PATH = "/Users/main/desktop/code/scraping/chromedriver_PATH_for_selenium_ref/chromedriver"
-        self.chrome_options = Options()
-        self.chrome_options.headless = True
-        self.chrome_options.add_argument("--disable-dev-shm-usage")
-        self.chrome_options.add_argument("--disable-extensions")
-        self.chrome_options.add_argument("--disable-gpu")
-        self.chrome_options.add_argument("--no-sandbox")
-        self.driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
-        self.actions = ActionChains(self.driver)
-        self.url = url
-        self.gv = None
+        #Chromedriver
+        try:
+            self.chrome_options = Options()
+            self.chrome_options.headless = True
+            self.chrome_options.add_argument("--disable-dev-shm-usage")
+            self.chrome_options.add_argument("--disable-extensions")
+            self.chrome_options.add_argument("--disable-gpu")
+            self.chrome_options.add_argument("--no-sandbox")
+            self.driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
+            self.actions = ActionChains(self.driver)
+            self.url = url
+            self.gv = None
+        
+        except:
+            pass
 
         try:
             self.get_info()
         
         except:
             pass
-
+        
+        #Variables
+        self.url = url
+            self.gv = None
 
     def get_info(self):
+        #Get PDP
         try:
             self.driver.get(self.url)
             self.driver.set_window_size(1440, 900)
